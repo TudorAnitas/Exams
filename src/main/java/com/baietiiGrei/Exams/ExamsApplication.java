3bd2a91495dfa2c207157428dc5ba17c38aa6b8d
Anitas Tudor <anitas_tudor@yahoo.com>
2020-04-04 20:25:35 +0300

    Created a homepage

--- src/main/java/com/baietiiGrei/Exams/ExamsApplication.java
+++ src/main/java/com/baietiiGrei/Exams/ExamsApplication.java
@@ -2,6 +2,10 @@ package com.baietiiGrei.Exams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class ExamsApplication {
@@ -10,4 +14,17 @@ public class ExamsApplication {
 		SpringApplication.run(ExamsApplication.class, args);
 	}
 
+	// controller used to check the url for a certain word
+	// show the home html page
+	@Controller
+	public class GreetingController {
+		// if the url ends in /home it shows the home.html
+		@GetMapping("/home")
+		public String greeting( Model model) {
+			model.addAttribute("name");
+			return "home";
+		}
+
+	}
 }
+
