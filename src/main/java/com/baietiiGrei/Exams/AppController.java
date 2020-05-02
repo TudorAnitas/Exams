c39bc3a07909b67361d2c16ce0fc650ee38c37a0
PaulBal <paul.baltarete99@e-uvt.ro>
2020-05-02 22:30:21 +0300

    Added filtering functionalities to all() method

--- src/main/java/com/baietiiGrei/Exams/AppController.java
+++ src/main/java/com/baietiiGrei/Exams/AppController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 public class AppController {
@@ -22,8 +22,20 @@ public class AppController {
     }
 
     @GetMapping("/exams")
-    public List<Exam> all(@RequestParam(name = "id") Long id) {
-        return (List<Exam>) repository.findAll();
+    public List<Exam> all(@RequestParam(required = false, name = "study_year") Integer studyYear, @RequestParam(required = false, name = "section") String section,
+        @RequestParam(required = false, name = "teacher") String teacher, @RequestParam(required = false, name = "classroom") String classroom) {
+
+        List<Exam> exams = repository.findAll();
+        if(studyYear != null)
+            exams = exams.stream().filter(e -> e.getStudy_year() == studyYear).collect(Collectors.toList());
+        if(section != null)
+            exams = exams.stream().filter(e -> e.getSection().equals(section)).collect(Collectors.toList());
+        if(teacher != null)
+            exams = exams.stream().filter(e -> e.getTeacher().equals(teacher)).collect(Collectors.toList());
+        if(classroom != null)
+            exams = exams.stream().filter(e -> e.getClassroom().equals(classroom)).collect(Collectors.toList());
+
+        return exams;
     }
 
     @PostMapping("/exams")
@@ -31,19 +43,14 @@ public class AppController {
         return ResponseEntity.ok(repository.save(exam));
     }
 
-    @PutMapping("/exams")
+    @PutMapping("/exams") // ?
     public ResponseEntity<String> updateExam(@RequestBody Exam exam) {
-        //repository.
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @RequestMapping("/test")
-    public String welcome(){
-        return "index2.jsp";
-    }
-    @RequestMapping("/home")
-    public String home(){
-        System.out.println("home is working");
-        return "home";
+    @DeleteMapping("/exams")
+    public ResponseEntity<String> deleteExam(@RequestParam(name = "id") Long id) {
+        repository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
